144f36f3a2c2025b18382374b04eb0974f873e18
okeen <eneko.taberna@gmail.com>
2011-12-01 14:56:07 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Etware2::Application.config.secret_token = '33d0a855af72bc126e0fa8a9080bb653b006403bd74991adf6a2f4ef33d800ca5922bbca04e454135ccc604161acadd59f648f1217edb480aeb106dedbefe143'
