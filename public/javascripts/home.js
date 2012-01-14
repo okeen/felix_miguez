3bb1d5e5fb31249e20b9dbad91d1d7ee825c0ad1
okeen <eneko.taberna@gmail.com>
2012-01-14 15:56:30 +0100

    Home mosaic shows and hides elements with tiomeout using cool anims

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,18 +1,35 @@
-$(function(){
-    var container = $('#mosaic');
-    container.imagesLoaded( function(){
+
+relayout = function(){
+    container.isotope({filter:  $(".no_one")});
+    setTimeout(function(){
+        changeImageWidth();
+        container.isotope({filter: $(".mosaic-image")});
+        setTimeout("relayout()", 5000);    
+    }, 600);
+    
+}
+changeImageWidth= function(){
         $(".mosaic-image").each(function(){
             var width=  (2^(Math.ceil(Math.random() *3) )) +1;
+            $(this).removeClass("grid_1 grid_2 grid_4");
             $(this).addClass("grid_" + width);
-            $(this).width($(this) -10);
+            $(this).width($(this).width -10);
         });
-        container.isotope({
+    
+}
+
+$(function(){
+    container = $('#mosaic');
+    container.imagesLoaded( function(){
+        changeImageWidth();
+        $('#mosaic').isotope({
             // options
             itemSelector : '.mosaic-image',
             layoutMode : 'masonry',
             columnWidth: 150
         });
+    
     });
-      
+    setTimeout("relayout()", 5000);  
 });
     
